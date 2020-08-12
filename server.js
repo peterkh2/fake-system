d9711949c832de0e1a9a4ad8d8f8b7dfd7dcf9e1
Peter Chan <65880423+peterkh2@users.noreply.github.com>
2020-08-12 15:13:23 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,49 +1,43 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var pg = require('pg');
+var fs = require('fs');
 var app = express();
 
 app.set('port', process.env.PORT || 5000);
 app.use(express.static('public'));
 app.use(bodyParser.json());
 
-app.post('/insert', function(req, res) {
-//     pg.connect(process.env.DATABASE_URL, function (err, conn, done) {
-//         if (err) console.log(err);
-//         conn.query(
-//             'INSERT INTO salesforce.Ruby_Survey__c (Full_Name__c, Position__c, Rating__c, Learnt_1__c, Learnt_2__c, Learnt_3__c, Suggestion_1__c, Suggestion_2__c, Suggestion_3__c) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)',
-//             [req.body.name.trim(), req.body.position.trim(), req.body.rating.trim(), req.body.learnt1.trim(), req.body.learnt2.trim(), req.body.learnt3.trim(), req.body.suggest1.trim(), req.body.suggest2.trim(), req.body.suggest3.trim()],
-//             function(err, result) {
-//                 done();
-//                 if (err) {
-//                     res.status(400).json({error: err.message});
-//                 }
-//                 else {
-//                     res.json(result);
-//                 }
-//             }
-//         );
-//     });
-});
-
-app.get('/all-servey', function(req, res) {
+app.get('/svc', function(request, response) {
+    var nameString = request.query.name;
+    var historyString = request.query.history;
     
-//     pg.connect(process.env.DATABASE_URL, function (err, conn, done) {
-//         if (err) console.log(err);
-//         conn.query(
-//             'SELECT * FROM salesforce.Ruby_Survey__c ORDER BY CreatedDate DESC',
-//             function(err, result) {
-//                 done();
-//                 if (err) {
-//                     res.status(400).json({error: err.message});
-//                 }
-//                 else {
-//                     //res.json(result);
-//                     res.send(res.json(result));
-//                 }
-//             }
-//         );
-//     });
+    var jsonContent = [{}];
+    
+    var outputResult = jsonContent;
+    const limit = request.query.limit;
+    if(limit && limit <= jsonContent.length){
+        outputResult = outputResult.slice(0, limit);
+    }
+    
+    response.setHeader('Access-Control-Allow-Origin','*');
+    response.send(JSON.parse(JSON.stringify(outputResult)));
+    
+});
+app.get('/internal-api', function(request, response) {
+    var nameString = request.query.name;
+    var historyString = request.query.history;
+    
+    var jsonContent = [{}];
+    
+    var outputResult = jsonContent;
+    const limit = request.query.limit;
+    if(limit && limit <= jsonContent.length){
+        outputResult = outputResult.slice(0, limit);
+    }
+    
+    response.setHeader('Access-Control-Allow-Origin','*');
+    response.send(JSON.parse(JSON.stringify(outputResult)));
     
 });
 
