546f26e37974bcfa0c45be9a07a848f9067cf831
Peter Chan <65880423+peterkh2@users.noreply.github.com>
2020-08-17 10:53:31 +0800

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,23 @@ app.set('port', process.env.PORT || 5000);
 app.use(express.static('public'));
 app.use(bodyParser.json());
 
+app.get('/customerSearch', function(request, response) {
+    var nameString = request.query.name;
+    var historyString = request.query.history;
+    
+    var jsonContent = [{}];
+    
+    var outputResult = jsonContent;
+    const limit = request.query.limit;
+    if(limit && limit <= jsonContent.length){
+        outputResult = outputResult.slice(0, limit);
+    }
+    
+    response.setHeader('Access-Control-Allow-Origin','*');
+    response.send(JSON.parse(JSON.stringify(outputResult)));
+    
+});
+
 app.get('/svc', function(request, response) {
     var nameString = request.query.name;
     var historyString = request.query.history;
